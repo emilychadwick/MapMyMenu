81cdaf3df5588695e0723d9214086e207f89b62c
Emily Chadwick <ekaseberg@gmail.com>
2017-01-24 18:36:20 -0800

    Lesson 4.1 fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
-  get 'products/index'
+  get 'products/index.html.erb'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
