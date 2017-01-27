cd8811a13404b814acd14e5813532385c0170ca6
Emily Chadwick <ekaseberg@gmail.com>
2017-01-26 17:19:05 -0800

    Lesson 4.1 final

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,5 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
-  get '/products'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
